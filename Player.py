3179818747385c4d246db2b16a0e7894fa7cd286
mahaveer <37685-mahaveer@users.noreply.gitlab.aicrowd.com>
2023-08-16 02:03:19 -0700

    handled key input

--- Player.py
+++ Player.py
@@ -1,3 +1,7 @@
+import time
+import sys
+from inputimeout import inputimeout, TimeoutOccurred
+
 class Player:
     def __init__(self, name=None):
         self.name = name
@@ -25,3 +29,20 @@ class Player:
         print("Enter Player Name: ")
         name = input()
         self.set_player_name(name)
+
+    # def wait_for_input(self, timeout):
+    #     start_time = time.time()
+    #     while time.time() - start_time < timeout:
+    #         input_value = input('Enter some input: ')
+    #         if input_value:
+    #             return input_value
+    #     else:
+    #         print('timedout : moving block down')
+    #         return None
+        
+    def wait_for_input(self, timeout):
+        try:
+            keystroke = inputimeout(prompt='Enter Input', timeout=timeout)
+        except TimeoutOccurred:
+            return 'd'
+        return keystroke
--- Tetris.py
+++ Tetris.py
@@ -9,33 +9,39 @@ from Board import *
 
 class Tetris:
     def __init__(self):
-        # self.player = self.initialize_player()
-        # self.board = self.initialize_board()
+        self.player = self.initialize_player()
+        self.board = self.initialize_board()
+        self.game_alive = True
+        self.player_input_timeout = 3 # 1 sec
         self.start_game()
 
     def initialize_player(self):
         player = Player()
-        player.ask_player_name()
-        # greet player
-        print("Hello {}! Welcome to Tetris".format(player.get_player_name()))
+        # player.ask_player_name()
+        # print("Hello {}! Welcome to Tetris".format(player.get_player_name()))
         return player
     
     def initialize_board(self):
         board = Board(height = 8, width = 8)
-        board.display_board()
         return board
 
     def wait_for_enter_key(self):
         print("Press Enter to start the game")
         input()
-        print("game started")
+        print("Game Started")
 
+    def pause_game(self):
+        pass
+    
     def start_game(self):
         self.wait_for_enter_key()
-        
+        while(self.game_alive):
+            val = self.player.wait_for_input(self.player_input_timeout)
+            if val=='':
+                self.game_alive = False
+            
 
-    
+        print("Game Stopped")
 
-    
 game = Tetris()
 
